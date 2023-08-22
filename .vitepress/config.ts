b32e31367976ca339c9a46a4b259ecc43933b1f5
yshrsmz <the.phantom.bane+github@gmail.com>
2023-08-23 00:24:31 +0900

    add adsense script

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -54,6 +54,15 @@ export default defineConfig({
         return t;
       })(document, 'script', 'twitter-wjs');`,
     ],
+    // adsense
+    [
+      'script',
+      {
+        async: '',
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1022395241918455',
+        crossorigin: 'anonymous',
+      },
+    ],
   ],
   themeConfig: {},
   markdown: {
