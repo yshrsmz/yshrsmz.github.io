b71845a8a9ba7233e20c39da0adec500c3b5e045
yshrsmz <the.phantom.bane@gmail.com>
2023-12-30 16:49:36 +0900

    add create-scrap-action
    add
    
    update

---
+++ packages/action-create-scrap/src/index.ts
@@ -0,0 +1,29 @@
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+async function run(): Promise<void> {
+  const repo = context.repo;
+
+  try {
+    const token = getInput("github_token", { required: true });
+    const issueNumber = Number(getInput("issue_number", { required: true }));
+
+    const octokit = getOctokit(token);
+
+    const { data, status } = await octokit.rest.issues.get({
+      owner: repo.owner,
+      repo: repo.repo,
+      issue_number: issueNumber,
+    });
+
+    console.log({ status, data });
+  } catch (error) {
+    if (error instanceof Error) {
+      setFailed(error.message);
+    } else {
+      setFailed(`Unknown error: ${error}`);
+    }
+  }
+}
+
+run();
